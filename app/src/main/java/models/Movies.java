182fc6ab0c5a94067a8c109d912fae7d7da97bf6
trilltrev <trevor1.taylor@famu.edu>
2020-09-16 14:43:48 -0400

    Initial commit

---
+++ app/src/main/java/models/Movies.java
@@ -0,0 +1,43 @@
+package models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movies {
+
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movies(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movies> fromJsonArray(JSONArray moviesJsonArray) throws JSONException {
+        List<Movies> movies = new ArrayList<>();
+          for (int i = 0; i < moviesJsonArray.length(); i++) {
+                   movies.add(new Movies(moviesJsonArray.getJSONObject(i)));
+          }
+          return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
+
+
